0a72247522f036064a059f4a6de000749aef54c3
Бороздин Дмитрий <borozdin1@yandex.ru>
2021-05-31 18:22:13 +0700

    Отредактирована 1я функция. Добавлена функция, возвращающая случайное число с плавающей точкой.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-const getRandomNumber = (min,max) => {
-  min = (min<0) ? 0 : min;
-  return Math.floor(Math.random()*(max - min + 1)) + min;
+const getRandomInteger = (min,max) => {
+  min = Math.abs(min);
+  max = Math.abs(max);
+  if (min>max) {
+    const temp = max;
+    max = min;
+    min = temp;
+  }
+  return Math.floor(Math.random()*(max-min+1)) + min;
 };
 
-getRandomNumber(10,70);
+getRandomInteger(10,2);
+
+const getRandomFloat = (min,max, decimal) => {
+  min = Math.abs(min);
+  max = Math.abs(max);
+  decimal = Math.abs(decimal);
+  if (min>max) {
+    const temp = max;
+    max = min;
+    min = temp;
+  }
+  return Math.round((Math.random()*(max-min) + min) *Math.pow(10,decimal))/ Math.pow(10,decimal);
+};
+
+getRandomFloat(1.5,-1.1, 2);
